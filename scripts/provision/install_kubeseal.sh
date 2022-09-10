5a9cd03b72bbc45f59079007f09fd51350e6abef
Szymon Osiecki <szymonos@outlook.com>
2022-09-10 11:03:37 +0200

    refactor: provision scripts

--- scripts/provision/install_kubeseal.sh
+++ scripts/provision/install_kubeseal.sh
@@ -4,13 +4,13 @@ scripts/provision/install_kubeseal.sh
 '
 
 APP='kubeseal'
-while [[ -z $REL ]]; do
+while [[ -z "$REL" ]]; do
   REL=$(curl -sk https://api.github.com/repos/bitnami-labs/sealed-secrets/releases/latest | grep -Po '"tag_name": *"v\K.*?(?=")')
 done
 
-if type $APP &>/dev/null; then
+if type "$APP" &>/dev/null; then
   VER=$(kubeseal --version | sed -r 's/.* ([0-9\.]+)$/\1/')
-  if [ $REL == $VER ]; then
+  if [ "$REL" == "$VER" ]; then
     echo "The latest $APP v$VER is already installed!"
     exit 0
   fi
@@ -18,4 +18,4 @@ fi
 
 echo "Install $APP v$REL"
 curl -Lsk "https://github.com/bitnami-labs/sealed-secrets/releases/download/v${REL}/kubeseal-${REL}-linux-amd64.tar.gz" | tar -xz
-install -m 0755 kubeseal /usr/bin/kubeseal && rm -f kubeseal
+install -o root -g root -m 0755 kubeseal /usr/local/bin/kubeseal && rm -f kubeseal
--- scripts/provision/install_omp.sh
+++ scripts/provision/install_omp.sh
@@ -4,18 +4,19 @@ scripts/provision/install_omp.sh
 '
 
 APP='oh-my-posh'
-while [[ -z $REL ]]; do
+while [[ -z "$REL" ]]; do
   REL=$(curl -sk https://api.github.com/repos/JanDeDobbeleer/oh-my-posh/releases/latest | grep -Po '"tag_name": *"v\K.*?(?=")')
 done
 
-if type $APP &>/dev/null; then
+if type "$APP" &>/dev/null; then
   VER=$(oh-my-posh version)
-  if [ $REL == $VER ]; then
+  if [ "$REL" == "$VER" ]; then
     echo "The latest $APP v$VER is already installed!"
     exit 0
   fi
 fi
 
 echo "Install $APP v$REL"
-curl -Lsk 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64' -o /usr/bin/oh-my-posh
-chmod +x /usr/bin/oh-my-posh
+curl -LsOk 'https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64'
+mkdir -p /opt/omp && install -o root -g root -m 0755 posh-linux-amd64 /opt/omp/oh-my-posh && rm -f posh-linux-amd64
+[ -f /usr/bin/oh-my-posh ] || ln -s /opt/omp/oh-my-posh /usr/bin/oh-my-posh
