e237d9f3a06a635e1ebc1fa59f9c0c7c262d63a6
Szymon Osiecki <szymonos@outlook.com>
2022-09-11 23:42:08 +0200

    feat: multi sys edge, gnome, xrdp installers

--- scripts/provision/install_gnome.sh
+++ scripts/provision/install_gnome.sh
@@ -3,6 +3,12 @@
 scripts/provision/install_gnome.sh
 '
 
-dnf group install -y gnome-desktop
-dnf install -y gnome-tweaks gnome-extensions-app
+# determine system id
+SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=\"?\K(fedora|debian|ubuntu|opensuse)' /etc/os-release)
+if [ "$SYS_ID" = 'fedora' ]; then
+  dnf group install -y gnome-desktop
+  dnf install -y gnome-tweaks gnome-extensions-app
+elif [ "$SYS_ID" = 'debian' ] || [ "$SYS_ID" = 'ubuntu' ]; then
+  DEBIAN_FRONTEND=noninteractive apt-get update && apt-get install -y ubuntu-desktop-minimal gnome-tweaks gnome-shell-extensions
+fi
 systemctl set-default graphical.target
--- scripts/provision/install_xrdp.sh
+++ scripts/provision/install_xrdp.sh
@@ -3,12 +3,20 @@
 scripts/provision/install_xrdp.sh
 '
 
-# Load the Hyper-V kernel module
-if ! [ -f "/etc/modules-load.d/hv_sock.conf" ] || [ "$(cat /etc/modules-load.d/hv_sock.conf | grep hv_sock)" = ""  ]; then
-  echo "hv_sock" | sudo tee -a /etc/modules-load.d/hv_sock.conf > /dev/null
+SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=\"?\K(fedora|debian|ubuntu|opensuse)' /etc/os-release)
+if [ "$SYS_ID" = 'fedora' ]; then
+  # Load the Hyper-V kernel module
+  if ! [ -f "/etc/modules-load.d/hv_sock.conf" ] || [ "$(cat /etc/modules-load.d/hv_sock.conf | grep hv_sock)" = "" ]; then
+    echo "hv_sock" | sudo tee -a /etc/modules-load.d/hv_sock.conf >/dev/null
+  fi
+  dnf -y install xrdp tigervnc-server
+  systemctl enable --now xrdp
+  # enable firewall rules
+  firewall-cmd --add-port=3389/tcp
+  firewall-cmd --runtime-to-permanent
+elif [ "$SYS_ID" = 'debian' ] || [ "$SYS_ID" = 'ubuntu' ]; then
+  apt-get update
+  DEBIAN_FRONTEND=noninteractive apt-get install -y xrdp tigervnc-standalone-server tigervnc-xorg-extension tigervnc-viewer
+  usermod -a -G ssl-cert xrdp
+  ufw allow 3389
 fi
-dnf -y install xrdp tigervnc-server
-systemctl enable --now xrdp
-
-firewall-cmd --add-port=3389/tcp
-firewall-cmd --runtime-to-permanent
