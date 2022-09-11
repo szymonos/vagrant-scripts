00c5cc9d93554fef085f6dba98bf96319c07b557
Szymon Osiecki <szymonos@outlook.com>
2022-09-11 23:42:08 +0200

    feat: multi sys edge, gnome, xrdp installers

--- scripts/provision/install_edge.sh
+++ scripts/provision/install_edge.sh
@@ -3,7 +3,20 @@
 scripts/provision/install_edge.sh
 '
 
-rpm --import 'https://packages.microsoft.com/keys/microsoft.asc'
-dnf config-manager --add-repo 'https://packages.microsoft.com/yumrepos/edge'
-mv /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-stable.repo
-dnf install -y microsoft-edge-stable
+# determine system id
+SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=\"?\K(fedora|debian|ubuntu|opensuse)' /etc/os-release)
+if [ "$SYS_ID" = 'fedora' ]; then
+  rpm --import 'https://packages.microsoft.com/keys/microsoft.asc'
+  dnf config-manager --add-repo 'https://packages.microsoft.com/yumrepos/edge'
+  mv -f /etc/yum.repos.d/packages.microsoft.com_yumrepos_edge.repo /etc/yum.repos.d/microsoft-edge-stable.repo
+  dnf install -y microsoft-edge-stable
+elif [ "$SYS_ID" = 'debian' ] || [ "$SYS_ID" = 'ubuntu' ]; then
+  [ -f /etc/apt/trusted.gpg.d/microsoft.gpg ] || curl -fsSLk https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor -o /etc/apt/trusted.gpg.d/microsoft.gpg
+  sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/edge stable main" > /etc/apt/sources.list.d/microsoft-edge-stable.list'
+  sudo apt update
+  sudo apt install -y microsoft-edge-stable
+elif [ "$SYS_ID" = 'opensuse' ]; then
+  sudo zypper addrepo https://packages.microsoft.com/yumrepos/edge edge
+  sudo zypper refresh
+  sudo zypper install -y microsoft-edge-stable
+fi
--- scripts/provision/install_pwsh.sh
+++ scripts/provision/install_pwsh.sh
@@ -22,13 +22,13 @@ SYS_ID=$(grep -oPm1 '^ID(_LIKE)?=\"?\K(fedora|debian|ubuntu|opensuse)' /etc/os-r
 if [ "$SYS_ID" = 'fedora' ]; then
   dnf install -y "https://github.com/PowerShell/PowerShell/releases/download/v$REL/powershell-$REL-1.rh.x86_64.rpm"
 elif [ "$SYS_ID" = 'debian' ] || [ "$SYS_ID" = 'ubuntu' ]; then
-  curl -Lsk "https://github.com/PowerShell/PowerShell/releases/download/v$REL/powershell_$REL-1.deb_amd64.deb" -o /tmp/powershell.deb
-  apt-get install -fy /tmp/powershell.deb && rm -f /tmp/powershell.deb
+  curl -Lsk "https://github.com/PowerShell/PowerShell/releases/download/v$REL/powershell_$REL-1.deb_amd64.deb" -o powershell.deb
+  dpkg -i powershell.deb && rm -f powershell.deb
 else
   [ "$SYS_ID" = 'opensuse' ] && zypper in -y libicu
-  curl -Lsk https://github.com/PowerShell/PowerShell/releases/download/v$REL/powershell-$REL-linux-x64.tar.gz -o /tmp/powershell.tar.gz
+  curl -Lsk https://github.com/PowerShell/PowerShell/releases/download/v$REL/powershell-$REL-linux-x64.tar.gz -o powershell.tar.gz
   mkdir -p /opt/microsoft/powershell/7
-  tar zxf /tmp/powershell.tar.gz -C /opt/microsoft/powershell/7 && rm -f /tmp/powershell.tar.gz
+  tar zxf powershell.tar.gz -C /opt/microsoft/powershell/7 && rm -f powershell.tar.gz
   chmod +x /opt/microsoft/powershell/7/pwsh
-  ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
+  [ -f /usr/bin/pwsh ] || ln -s /opt/microsoft/powershell/7/pwsh /usr/bin/pwsh
 fi
