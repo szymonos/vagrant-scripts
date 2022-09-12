91a7061f5afd2ecad7eefc089b98dbd13b8c5240
Szymon Osiecki <szymonos@outlook.com>
2022-09-13 00:06:14 +0200

    fix(ps): set_ssh_config
    add to ssh config with new line

--- scripts/trigger/set_ssh_config.ps1
+++ scripts/trigger/set_ssh_config.ps1
@@ -28,20 +28,25 @@ Host $HostName
 "@
 
 Write-Host "Adding $HostName to ssh config..."
-if (-not (Test-Path $sshConfig)) { New-Item $sshConfig -ItemType File -Force }
-if (Select-String -Pattern "HostName $IpAddress" -Path $sshConfig) {
-    # update Host if HostName entry already present
-    $content = [IO.File]::ReadAllText($sshConfig) -replace "Host[^\n]+\n[^\n]+$IpAddress\n[\s\S]+?(?=(\nHost|\z))", $vagrantConfig
-    [IO.File]::WriteAllText($sshConfig, $content)
-} elseif (Select-String -Pattern "Host $HostName" -Path $sshConfig) {
-    # update HostName if Host entry already present
-    $content = [IO.File]::ReadAllText($sshConfig) -replace "Host $HostName[\s\S]+?(?=(\nHost|\z))", $vagrantConfig
-    [IO.File]::WriteAllText($sshConfig, $content)
+if (Test-Path $sshConfig -PathType Leaf) {
+    $content = [IO.File]::ReadAllText($sshConfig).TrimEnd()
+    if (Select-String -Pattern "HostName $IpAddress" -Path $sshConfig) {
+        # update Host if HostName entry already present
+        $content = $content -replace "Host[^\n]+\n[^\n]+$IpAddress\n[\s\S]+?(?=(\nHost|\z))", $vagrantConfig
+        [IO.File]::WriteAllText($sshConfig, $content)
+    } elseif (Select-String -Pattern "Host $HostName" -Path $sshConfig) {
+        # update HostName if Host entry already present
+        $content = $content -replace "Host $HostName[\s\S]+?(?=(\nHost|\z))", $vagrantConfig
+        [IO.File]::WriteAllText($sshConfig, $content)
+    } else {
+        [IO.File]::WriteAllText($sshConfig, "$content`n$vagrantConfig")
+    }
 } else {
-    Add-Content -Value $vagrantConfig -Path $sshConfig
+    New-Item $sshConfig -ItemType File -Force
+    [IO.File]::WriteAllText($sshConfig, $vagrantConfig)
 }
 
-if (Test-Path $knownHosts) {
+if (Test-Path $knownHosts -PathType Leaf) {
     Write-Host 'Cleaning ssh known_hosts file...'
     if (Select-String -Pattern "^$IpAddress" -Path $knownHosts) {
         $content = [IO.File]::ReadAllLines($knownHosts) -notmatch "^$IpAddress"
