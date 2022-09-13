9b621aa0f1e1fe1412e0ee35a3c873b3965dd74f
Szymon Osiecki <szymonos@outlook.com>
2022-09-13 18:59:35 +0000

    fix(sh): install_helm

--- .assets/provision/install_helm.sh
+++ .assets/provision/install_helm.sh
@@ -3,6 +3,6 @@
 .assets/provision/install_helm.sh
 '
 
-curl -fsSLk -o get_helm.sh 'https://raw.githubusercontent.com/helm/helm/main/.assets/get-helm-3'
+curl -fsSLk -o get_helm.sh 'https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3'
 chmod 700 get_helm.sh
 ./get_helm.sh && rm -f get_helm.sh
