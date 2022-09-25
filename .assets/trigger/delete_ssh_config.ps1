feda8cd170714f37576f120301bcef4c9f6b822c
Szymon Osiecki <szymonos@outlook.com>
2022-09-25 13:27:48 +0200

    refactor(ps): ssh_config triggers

--- .assets/trigger/delete_ssh_config.ps1
+++ .assets/trigger/delete_ssh_config.ps1
@@ -21,24 +21,19 @@ param (
 
 $sshConfig = "$HOME/.ssh/config"
 if (Test-Path $sshConfig -PathType Leaf) {
-    Write-Host "Removing $HostName from ssh config..."
     $content = [IO.File]::ReadAllText($sshConfig).TrimEnd()
-    if (Select-String -Pattern "HostName $IpAddress" -Path $sshConfig) {
-        # update Host if HostName entry already present
-        $content = $content -replace "Host[^\n]+\n[^\n]+$IpAddress\n[\s\S]+?(?=(\nHost|\z))"
-        [IO.File]::WriteAllText($sshConfig, $content)
-    } elseif (Select-String -Pattern "Host $HostName" -Path $sshConfig) {
-        # update HostName if Host entry already present
+    if ($content | Select-String -Pattern "Host $HostName") {
+        Write-Host "Removing '$HostName' entry from ssh config..."
         $content = $content -replace "Host $HostName[\s\S]+?(?=(\nHost|\z))"
-        [IO.File]::WriteAllText($sshConfig, $content)
+        [IO.File]::WriteAllText($sshConfig, $content.TrimEnd())
     }
 }
 
 $knownHosts = "$HOME/.ssh/known_hosts"
 if (Test-Path $knownHosts -PathType Leaf) {
-    Write-Host 'Cleaning ssh known_hosts file...'
-    if (Select-String -Pattern "^$IpAddress" -Path $knownHosts) {
-        $content = [IO.File]::ReadAllLines($knownHosts) -notmatch "^$IpAddress"
-        [IO.File]::WriteAllLines($knownHosts, $content)
+    $content = [IO.File]::ReadAllLines($knownHosts)
+    if ($content | Select-String -Pattern "^$IpAddress") {
+        Write-Host "Removing '$IpAddress' fingerprint from ssh known_hosts..."
+        [IO.File]::WriteAllLines($knownHosts, ($content -notmatch "^$IpAddress"))
     }
 }
