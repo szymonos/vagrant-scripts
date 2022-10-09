26aeccb56cfa1cd5ffce46d3e17c215672ddba65
Szymon Osiecki <szymonos@outlook.com>
2022-10-09 21:10:18 +0200

    refactor(sh): provision scripts

--- .assets/provision/set_authorized_keys.sh
+++ .assets/provision/set_authorized_keys.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
-: '
-.assets/provision/set_authorized_keys.sh
-'
 # check if the passed parameter starts with rsa phrase
-if ! echo "$1" | grep -E '^(ssh-|ecdsa-)' &>/dev/null; then
+if ! echo "$1" | grep -qE '^(ssh-|ecdsa-)'; then
   echo -e '\e[91mMissing ssh public key in the script parameter!'
   exit 1
 fi
 
-grep "$1" ~/.ssh/authorized_keys &>/dev/null || echo "$1" >>~/.ssh/authorized_keys
+grep -q "$1" ~/.ssh/authorized_keys || echo "$1" >>~/.ssh/authorized_keys
