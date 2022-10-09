2d36e35cc4241359003f56a3a2630909ca1da135
Szymon Osiecki <szymonos@outlook.com>
2022-10-09 08:43:28 +0200

    refactor(sh): sudoers_nopasswd

--- .assets/scripts/sudoers_nopasswd.sh
+++ .assets/scripts/sudoers_nopasswd.sh
@@ -12,16 +12,14 @@ user=$USER
 
 if [[ "$1" = 'remove' ]]; then
   sudo rm -f "/etc/sudoers.d/$USER"
-else
+  sudo rm -f /usr/share/polkit-1/rules.d/49-nopasswd_global.rules
+elif id -nG "$user" | grep -qw 'wheel'; then
+  # disable sudo password prompt for current user
   cat <<EOF | sudo tee /etc/sudoers.d/$user >/dev/null
 $user ALL=(root) NOPASSWD: ALL
 EOF
-fi
-
-if [[ "$1" = 'remove' ]]; then
-  sudo rm -f /usr/share/polkit-1/rules.d/49-nopasswd_global.rules
-else
-[[ -d /usr/share/polkit-1/rules.d ]] && cat <<EOF | sudo tee /usr/share/polkit-1/rules.d/49-nopasswd_global.rules >/dev/null
+  # disable password in desktop environment
+  [[ -d /usr/share/polkit-1/rules.d ]] && cat <<EOF | sudo tee /usr/share/polkit-1/rules.d/49-nopasswd_global.rules >/dev/null
 /* Allow members of the wheel group to execute any actions
  * without password authentication, similar to "sudo NOPASSWD:"
  */
