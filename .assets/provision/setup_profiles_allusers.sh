0beed36d9f392b381ffe5ca22bf6df620f2ce17a
Szymon Osiecki <szymonos@outlook.com>
2022-10-09 21:10:18 +0200

    refactor(sh): provision scripts

--- .assets/provision/setup_profiles_allusers.sh
+++ .assets/provision/setup_profiles_allusers.sh
@@ -4,13 +4,27 @@ sudo .assets/provision/setup_profiles_allusers.sh
 '
 # get PowerShell profile path
 PROFILE_PATH=$(pwsh -nop -c '[IO.Path]::GetDirectoryName($PROFILE.AllUsersAllHosts)')
+SCRIPTS_PATH=$(pwsh -nop -c '$env:PSModulePath.Split(":")[1].Replace("Modules", "Scripts")')
 
 # *Copy global profiles
 if [ -d /tmp/config ]; then
+  # bash aliases
   \mv -f /tmp/config/bash_* /etc/profile.d/
+  # oh-my-posh profile
   \mv -f /tmp/config/theme.omp.json /etc/profile.d/
+  # PowerShell profile
   \mv -f /tmp/config/profile.ps1 $PROFILE_PATH
-  mkdir -p /usr/local/share/powershell/Scripts/ && \mv -f /tmp/config/ps_aliases_*.ps1 /usr/local/share/powershell/Scripts/
+  # PowerShell functions
+  \mv -f /tmp/config/ps_aliases_common.ps1 $SCRIPTS_PATH
+  # git functions
+  if type git &>/dev/null; then
+    \mv -f /tmp/config/ps_aliases_git.ps1 $SCRIPTS_PATH
+  fi
+  # kubectl functions
+  if type kubectl &>/dev/null; then
+    \mv -f /tmp/config/ps_aliases_kubectl.ps1 $SCRIPTS_PATH
+  fi
+  # clean config folder
   rm -fr /tmp/config
 fi
 
@@ -24,28 +38,22 @@ Write-Host 'Install PSReadLine' && Install-PSResource -Name PSReadLine -Scope Al
 Write-Host 'Install posh-git' && Install-PSResource -Name posh-git -Scope AllUsers -Quiet;
 Write-Host 'Enable ExperimentalFeature' && Enable-ExperimentalFeature PSAnsiRenderingFileInfo, PSNativeCommandArgumentPassing
 EOF
-# add powershell kubectl autocompletion
-if type kubectl &>/dev/null; then
-  cat <<'EOF' | pwsh -nop -c -
-(kubectl completion powershell).Replace("'kubectl'", "'k'") > $PROFILE.AllUsersCurrentHost
-EOF
-fi
 
 # *bash profile
 # add common bash aliases
-grep 'd/bash_aliases' ~/.bashrc &>/dev/null || cat <<'EOF' >>~/.bashrc
+grep -qw 'd/bash_aliases' ~/.bashrc || cat <<'EOF' >>~/.bashrc
 # common aliases
 [ -f /etc/profile.d/bash_aliases ] && source /etc/profile.d/bash_aliases
 EOF
 # add oh-my-posh invocation to .bashrc
-grep 'oh-my-posh' ~/.bashrc &>/dev/null || cat <<'EOF' >>~/.bashrc
+grep -qw 'oh-my-posh' ~/.bashrc || cat <<'EOF' >>~/.bashrc
 # initialize oh-my-posh prompt
 if type oh-my-posh &>/dev/null; then
   [ -f /etc/profile.d/theme.omp.json ] && eval "$(oh-my-posh --init --shell bash --config /etc/profile.d/theme.omp.json)"
 fi
 EOF
 # make path autocompletion case insensitive
-grep 'ignore-case on' /etc/inputrc &>/dev/null || echo 'set completion-ignore-case on' >>/etc/inputrc
+grep -qw 'completion-ignore-case' /etc/inputrc || echo 'set completion-ignore-case on' >>/etc/inputrc
 
 # *set localtime to UTC
 [ -f /etc/localtime ] || ln -s /usr/share/zoneinfo/UTC /etc/localtime
--- .assets/provision/setup_profiles_user.sh
+++ .assets/provision/setup_profiles_user.sh
@@ -9,15 +9,21 @@ $WarningPreference = 'Ignore';
 Set-PSResourceRepository -Name PSGallery -Trusted;
 Enable-ExperimentalFeature PSAnsiRenderingFileInfo, PSNativeCommandArgumentPassing
 EOF
+# add powershell kubectl autocompletion
+if type -f kubectl &>/dev/null; then
+  cat <<'EOF' | pwsh -nop -c -
+(kubectl completion powershell).Replace("'kubectl'", "'k'") >$PROFILE
+EOF
+fi
 
 # *bash profile
 # add common bash aliases
-grep 'd/bash_aliases' ~/.bashrc &>/dev/null || cat <<'EOF' >>~/.bashrc
+grep -qw 'd/bash_aliases' ~/.bashrc || cat <<'EOF' >>~/.bashrc
 # common aliases
 [ -f /etc/profile.d/bash_aliases ] && source /etc/profile.d/bash_aliases
 EOF
 # add oh-my-posh invocation to .bashrc
-grep 'oh-my-posh' ~/.bashrc &>/dev/null || cat <<'EOF' >>~/.bashrc
+grep -qw 'oh-my-posh' ~/.bashrc || cat <<'EOF' >>~/.bashrc
 # initialize oh-my-posh prompt
 if type oh-my-posh &>/dev/null; then
   [ -f /etc/profile.d/theme.omp.json ] && eval "$(oh-my-posh --init --shell bash --config /etc/profile.d/theme.omp.json)"
