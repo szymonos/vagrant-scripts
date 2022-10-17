2d2462181b7e9c6b3c99c368f594addaf9927d88
Szymon Osiecki <szymonos@outlook.com>
2022-10-17 09:50:08 +0200

    fix: apt install

--- .assets/provision/install_kubectl.sh
+++ .assets/provision/install_kubectl.sh
@@ -36,15 +36,13 @@ EOF
   dnf install -y kubectl
   ;;
 debian | ubuntu)
-  apt-get update
-  apt-get install -y apt-transport-https ca-certificates curl
+  apt-get update && apt-get install -y apt-transport-https ca-certificates curl
   # download the Google Cloud public signing key
   curl -fsSLk -o /usr/share/keyrings/kubernetes-archive-keyring.gpg 'https://packages.cloud.google.com/apt/doc/apt-key.gpg'
   # add the Kubernetes apt repository
   [ -f /etc/apt/sources.list.d/kubernetes.list ] || echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | tee /etc/apt/sources.list.d/kubernetes.list
   # update apt package index with the new repository and install kubectl
-  apt-get update
-  apt-get install -y kubectl
+  apt-get update && apt-get install -y kubectl
   ;;
 *)
   while [[ ! -f kubectl ]]; do
